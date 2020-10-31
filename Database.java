b3cccfd60f0886fe5f1530a05599d001bd3bb2a2
Debeshee <debesheedas@gmail.com>
2020-10-31 18:47:59 +0530

    nothing

--- Database.java
+++ Database.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.*;
 
 // Driver path   -------->   ".;JDBC/sqlite-jdbc-3.7.2.jar"
 //Driver path ----> For Non - Windows  ".:JDBC/sqlite-jdbc-3.7.2.jar"
@@ -7,12 +8,33 @@ public class Database
 {
     private String db_name = "ATM.db";
     private String url = "jdbc:sqlite:db/"+db_name; // url of the database
-    private ATM atm;
+    //private ATM atm;
 
-    Database(ATM atm){
+    /*Database(ATM atm){
       this.atm = atm;;
       db_name = "ATM.db";
       url = "jdbc:sqlite:db/"+db_name;
-   }
+   }*/
+
+   private Connection connect() {
+    Connection conn = null;
+    try {
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection(url);
+    } catch (Exception e) {
+        System.out.println(e.getClass().getName() + ": " + e.getMessage());
+    }
+    return conn;
+}
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello");
+        Database DB = new Database();
+        DB.connect();
+    }
     
 }
+
+    
+
